b9bee9d20a3f546065340ba4b0f0996a79d571cc
Apoorva <etoosapoorva123@gmail.com>
2022-08-29 00:46:05 +0530

    serve static files

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
 let express = require('express');
 let app = express();
 
+app.use("/public",express.static("public"));
+
 app.get("/",function(req,res)
 {
     res.sendFile(__dirname+"/views/index.html",function(req,res)
@@ -8,6 +10,9 @@ app.get("/",function(req,res)
         console.log("File successfully sent");
     });
 });
+
+
+// console.log("myapp.js");
 
 
 
